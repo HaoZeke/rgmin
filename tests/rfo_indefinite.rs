495374b01786135b2a5d9297c62dfdb010197dbd
Rohit Goswami <rgoswami@ieee.org>
2026-09-07 02:24:31 -0500

    test(rfo): construct the Newton session with RFO kind

--- tests/rfo_indefinite.rs
+++ tests/rfo_indefinite.rs
@@ -57,7 +57,9 @@ fn constrained_rfo_keeps_every_trial_inside_the_trust_and_box() {
         |x: ArrayView1<f64>| array![[3.0 * x[0] * x[0] - 1.0]],
     );
     let mut solver = Solver::new(
-        Method::Rfo,
+        Method::Newton {
+            kind: NewtonKind::Rfo,
+        },
         Control {
             maxiter: 1,
             gtol: 1e-12,
@@ -92,7 +94,9 @@ fn inactive_constraints_preserve_the_rfo_model() {
         |_x: ArrayView1<f64>| array![[2.0]],
     );
     let mut solver = Solver::new(
-        Method::Rfo,
+        Method::Newton {
+            kind: NewtonKind::Rfo,
+        },
         Control {
             maxiter: 1,
             gtol: 1e-12,
